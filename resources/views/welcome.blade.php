142fb66007e713c24775518cbdfb8d458c136438
QuyLV <quylv@splus-software.com.vn>
2024-06-24 15:36:09 +0700

    lesson 4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,13 +9,35 @@
             <div x-show="records.length"> Some Conetent</div>
         </div> --}}
 
-        <div x-data="{number: 0}">
+        {{-- <div x-data="{number: 0}">
             <button x-on:click="number++"> Increnemt</button>
             <div x-text="number">
             </div>
             <div x-show=" number >= 10">
                 that's is big number
             </div>
+        </div> --}}
+
+
+        {{-- lesson 4 --}}
+        {{-- <form x-data="{ name: ''}" x-on:submit.prevent="console.log(name)">
+            <input type="text" x-model="name">
+            <button type="submit">Submit</button>
+        </form>
+        
+        <div 
+            x-data = "{
+                name: '',
+                makeNameUppercase (){
+                    this.name= this.name.toUpperCase()
+                }
+
+            }"
+        >
+            <input type="text" x-model="name">
+            <span x-text="name"></span>
+            <button type="button" x-on:click="makeNameUppercase"> Uppercase</button>
         </div>
+         --}}
     </body>
 </html>
